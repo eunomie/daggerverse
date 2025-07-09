c46199a27b36fd39d58cc5fc3a2a51890a5b65ce
Yves Brissaud <yves.brissaud@gmail.com>
2025-07-09 10:12:25 +0200

    display readme
    Signed-off-by: Yves Brissaud <yves.brissaud@gmail.com>

--- glow/main.go
+++ glow/main.go
@@ -3,6 +3,10 @@
 package main
 
 import (
+	"context"
+	"fmt"
+
+	"dagger/glow/internal/dagger"
 	"github.com/charmbracelet/glamour"
 )
 
@@ -11,4 +15,17 @@ type Glow struct{}
 // Render a markdown input string to be displayed on a terminal.
 func (m *Glow) DisplayMarkdown(str string) (string, error) {
 	return glamour.Render(str, "dark")
+}
+
+// Print readme file in the terminal
+func (m *Glow) ReadMe(
+	ctx context.Context,
+// +defaultPath="README.md"
+	file dagger.File,
+) (string, error) {
+	c, err := file.Contents(ctx)
+	if err != nil {
+		return "", fmt.Errorf("could not read README.md: %w", err)
+	}
+	return m.DisplayMarkdown(c)
 }
